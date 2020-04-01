a1a78334db0f0a981afa74bd691db6a9b383f04a
themindstorm <parth.kabra@gmail.com>
2020-04-01 18:20:00 +0800

    torll

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-let list = [
-  {
-    id: 0,
-    url: 'https://www.youtube.com/watch?v=XqZsoesa55w&t=26s'
-  },
-  {
-    id: 1,
-    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-  },
-  {
-    id: 3,
-    url: 'https://www.youtube.com/watch?v=XqZsoesa55w&t=26s',
-  }
-]
+
+// RickRoll!!
+const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 
 let linkClicker = document.getElementById('link_click');
 
 window.onload = window.onfocus = () => {
-  let size = list.length;
-  let theChosenOne = Math.round(Math.random()*size);
-  console.log(theChosenOne);
-  let theChosenLink = list[theChosenOne].url;
-  console.log(theChosenLink);
-  this.document.location.href = theChosenLink;
+  this.document.location.href = url;
 }
